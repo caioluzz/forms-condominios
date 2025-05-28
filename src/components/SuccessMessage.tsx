56a026b22561796b39ed6c966bccb28d6ad42df4
caioluzz <115850065+caioluzz@users.noreply.github.com>
2025-05-28 09:07:50 -0300

    Update SuccessMessage.tsx

--- src/components/SuccessMessage.tsx
+++ src/components/SuccessMessage.tsx
@@ -12,16 +12,16 @@ const SuccessMessage: React.FC<SuccessMessageProps> = ({ estabelecimento }) => {
         <CheckCircle className="h-10 w-10 text-trenergia-green" />
       </div>
 
-      <h2 className="text-2xl font-bold text-gray-800 mb-2">
+      <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">
         Recebemos seus dados!
       </h2>
 
-      <p className="text-gray-600 text-center mb-6 max-w-sm text-balance">
+      <p className="text-gray-600 dark:text-gray-300 text-center mb-6 max-w-sm text-balance">
         Obrigado pelo seu interesse nas soluções da TR Energia. Nossa equipe entrará em contato em até 24 horas uteis com a proposta de economia na conta de energia.
       </p>
 
-      <div className="bg-trenergia-gray p-4 rounded-lg max-w-sm">
-        <p className="text-sm text-gray-600 flex items-start">
+      <div className="bg-trenergia-gray dark:bg-gray-800 p-4 rounded-lg max-w-sm">
+        <p className="text-sm text-gray-600 dark:text-gray-300 flex items-start">
           <span className="text-trenergia-green mr-2">•</span>
           <span>
             <span className="font-medium">Próximos passos:</span> Elaboraremos a proposta de economia de energia para que você possa aproveitar o seu desconto exclusivo.
