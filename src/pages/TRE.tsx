6bb0163d1c09f9f92857704ab3043ce71a2ebbe4
caioluzz <115850065+caioluzz@users.noreply.github.com>
2025-07-11 10:49:43 -0300

    adicionando oab

---
+++ src/pages/TRE.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LeadForm from '@/components/LeadForm';
+
+const TRE: React.FC = () => {
+  return (
+    <LeadForm type="tre" />
+  );
+};
+
+export default TRE; 
