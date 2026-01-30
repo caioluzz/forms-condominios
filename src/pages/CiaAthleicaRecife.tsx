3cc87dd2ee6831023fbb8f5882a49125935256e7
caioluzz <115850065+caioluzz@users.noreply.github.com>
2026-01-30 14:37:03 -0300

    novos formularios
    Cia Athletica Recife; Edf Praia de Gamboa; Cond Praia da Pipa

---
+++ src/pages/CiaAthleicaRecife.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LeadForm from '@/components/LeadForm';
+
+const CiaAthleicaRecife: React.FC = () => {
+  return <LeadForm type="cia_athletica_recife" />;
+};
+
+export default CiaAthleicaRecife; 
---
+++ src/pages/CondPraiaDaPipa.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LeadForm from '@/components/LeadForm';
+
+const CondPraiaDaPipa: React.FC = () => {
+  return <LeadForm type="cond_praia_da_pipa" />;
+};
+
+export default CondPraiaDaPipa; 
